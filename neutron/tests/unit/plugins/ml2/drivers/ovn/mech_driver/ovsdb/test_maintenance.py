236c10d21b91d03acd1f7e139b0165047824e177
Rodolfo Alonso Hernandez <ralonsoh@redhat.com>
2025-09-19 14:48:29 +0000

    [OVN] Remove the check for external ports support
    The support for ``HA_Chassis_Group`` in the ``Logical_Switch_Ports`` was
    provided in OVN v20.03.0 [1] and thus the support for external ports
    (baremetal nodes, SR-IOV).
    
    [1]https://github.com/ovn-org/ovn/commit/b31c76000bef314b68e776d318d1ce4cf152450b
    
    Signed-off-by: Rodolfo Alonso Hernandez <ralonsoh@redhat.com>
    Change-Id: I926db2b9e53a7b127d85cf8b6337e1632aebaf57

--- neutron/tests/unit/plugins/ml2/drivers/ovn/mech_driver/ovsdb/test_maintenance.py
+++ neutron/tests/unit/plugins/ml2/drivers/ovn/mech_driver/ovsdb/test_maintenance.py
@@ -805,7 +805,6 @@ class TestDBInconsistenciesPeriodics(testlib_api.SqlTestCaseLight,
     def _test_check_baremetal_ports_dhcp_options(self, dhcp_disabled=False):
         cfg.CONF.set_override('disable_ovn_dhcp_for_baremetal_ports',
                               dhcp_disabled, group='ovn')
-        self.fake_ovn_client.is_external_ports_supported.return_value = True
         nb_idl = self.fake_ovn_client._nb_idl
         self.fake_ovn_client._get_port_options.return_value = 'fake-port-opts'
 
